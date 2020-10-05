c249c7aa28acd98b7b59de7bd2cb4e7680d895a9
Fetra Ramanantsoa <gl.prout.peacemaker@gmail.com>
2020-10-05 12:01:41 +0300

    error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_mongoengine import MongoEngine
 from flask_bcrypt import Bcrypt
 
@@ -8,6 +8,14 @@ bcrypt = Bcrypt(app)
 app.config.from_pyfile('app-config.cfg')
 
 db.init_app(app)
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
+
+@app.errorhandler(500)
+def internal_error(e):
+    return jsonify(error=str(e)), 500
 
 if __name__ == '__main__':
     app.run()
